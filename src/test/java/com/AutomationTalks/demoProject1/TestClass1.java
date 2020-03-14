203a96b4dc731aa0de5c7b6674a85efb1c23450b
Pranay Dua <pranay.dua@macbook-pro-97.lan>
2020-03-14 12:26:02 -0700

    Browserstack support

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -18,15 +18,30 @@ import org.testng.annotations.Test;
 public class TestClass1 {
 
 	
-	public static WebDriver driver;
+	  public static WebDriver driver;
+	  public static final String USERNAME = "pranaydua3";
+	  public static final String AUTOMATE_KEY = "JpsuaBcnGANeykgafJe5";
+	  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 	@BeforeMethod
 	public void launchDriver() throws MalformedURLException {
-		DesiredCapabilities dr=null;
-		dr=DesiredCapabilities.chrome();
-		dr.setBrowserName("chrome");
-		dr.setPlatform(Platform.MAC);
-		System.setProperty("webdriver.chrome.driver", "/Users/pranay.dua/Downloads/chromedriver");
-		driver=new RemoteWebDriver(new URL("http://localhost:4545/wd/hub"),dr);
+		DesiredCapabilities caps=new DesiredCapabilities();
+//		dr=DesiredCapabilities.chrome();
+//		dr.setBrowserName("chrome");
+//		dr.setPlatform(Platform.MAC);
+//		System.setProperty("webdriver.chrome.driver", "/Users/pranay.dua/Downloads/chromedriver");
+//		driver=new RemoteWebDriver(new URL("http://localhost:4545/wd/hub"),dr);
+		
+		caps.setCapability("browser", "Firefox");
+	    caps.setCapability("browser_version", "60.0");
+	    caps.setCapability("os", "Windows");
+	    caps.setCapability("os_version", "7");
+	    caps.setCapability("resolution", "1024x768");
+	    caps.setCapability("project", "AutomationTalksProject");
+	    caps.setCapability("build", "Build1");
+	    caps.setCapability("name", "TestCaseName");
+//	    caps.setCapability("name", "Bstack-[Java] Sample Test");
+
+	     driver = new RemoteWebDriver(new URL(URL), caps);
 		//driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
